bf6abe24ff5bf1c233cee5553322fe5469eebf18
SDEBEUL <SDEBEUL@volvocars.com>
2016-03-02 09:08:39 +0100

    +cleaned up

--- C3G_Timesync/Program.cs
+++ C3G_Timesync/Program.cs
@@ -8,20 +8,92 @@ using System.Data.SqlClient;
 using System.Globalization;
 using System.Reflection;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 
 
 namespace C3G_Timesync
 {
+
+    static class Buffer
+    {
+        static List<string> _Logbuffer; // Static List instance
+        static Buffer() { _Logbuffer = new List<string>(); }
+        public static void Record(string value) { _Logbuffer.Add(value); }
+        public static void Delete(string value) { _Logbuffer.Remove(value); }
+        public static Int32 Count() { return _Logbuffer.Count(); }
+        public static List<string> getbuffer() { return _Logbuffer; }
+        public static void Display() { foreach (var value in _Logbuffer) { Console.WriteLine(value); } }
+        public static bool Contains(string file) { if (_Logbuffer.Contains(file)) { return true; } else { return false; } }
+    }
+
+    public class ConsoleSpiner
+    {
+        int counter;
+        public ConsoleSpiner()
+        {
+            counter = 0;
+        }
+        public void Turn()
+        {
+            counter++;
+            switch (counter % 4)
+            {
+                case 0: Console.Write("/"); break;
+                case 1: Console.Write("-"); break;
+                case 2: Console.Write("\\"); break;
+                case 3: Console.Write("-"); break;
+            }
+            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+        }
+    } 
+
+    static class Debug
+    {
+        public static void Init()
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener("C3g_TimesyncDebug.log"));
+            Trace.AutoFlush = true;
+            Trace.Indent();
+            Trace.Unindent();
+            Trace.Flush();
+        }
+        public static void Restart()
+        {
+            Console.WriteLine("System will restart in 10 seconds");
+            System.Threading.Thread.Sleep(10000);
+            var fileName = Assembly.GetExecutingAssembly().Location;
+            System.Diagnostics.Process.Start(fileName);
+            Environment.Exit(0);
+        }
+        public static void Message(string ls_part, string ls_message)
+        {
+            Trace.WriteLine("DT: " + System.DateTime.Now + " P: " + ls_part + " M: " + ls_message);
+            Console.WriteLine("DT: " + System.DateTime.Now + " P: " + ls_part + " M: " + ls_message);
+            using (EventLog eventlog = new EventLog("Application"))
+            {
+                eventlog.Source = "Application";
+                eventlog.WriteEntry(ls_message, EventLogEntryType.Information, 101, 1);
+            }
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Title = "VOLVO Comau C3G Timesync Build by SDEBEUL";
+            //build logtracer
+            Trace.Listeners.Add(new TextWriterTraceListener("C3g_TimesyncDebug.log"));
+            Debug.Init();
+            Debug.Message("INFO", "System restarted");
+            //
+            Console.Title = "VOLVO Comau C3G Timesync Build by SDEBEUL 16w08d03";
             Console.BufferHeight = 25;
             //
             Console.WriteLine("Gadata up and running current timestamp= " +  GetTimeFromGadata().ToString());
             //build file sytem watch 
+            //*****************************************************************************************************************************************
             FileSystemWatcher watcher = new FileSystemWatcher();
             watcher.Path = @"\\gnl9011101\6308-APP-NASROBOTBCK0001\robot_ga\ROBLAB\99999R99\Tsync";
             watcher.Filter = "*.TSF"; ;
@@ -30,58 +102,59 @@ namespace C3G_Timesync
             watcher.Created += new FileSystemEventHandler(OnChanged);
             watcher.Deleted += new FileSystemEventHandler(OnDeleted);
             watcher.EnableRaisingEvents = true;
-            //stop from closing
-            Console.ReadLine();
-            Console.Write("\b \b"); //Test 
-
-        }
-
-
-        // Event handeler for error event in wacther (auto restart)
-        private static void OnError(object source, ErrorEventArgs e)
-        {
-
-            Trace.Listeners.Add(new TextWriterTraceListener("C3g_TimesyncDebug.log"));
-            Trace.AutoFlush = true;
-            Trace.Indent();
-            Trace.Unindent();
-            Trace.Flush();
-            //  Show that an error has been detected.
-            Console.WriteLine("The FileSystemWatcher has detected an error");
-            Trace.WriteLine("The FileSystemWatcher has detected an error");
-            //  Give more information if the error is due to an internal buffer overflow. 
-            if (e.GetException().GetType() == typeof(InternalBufferOverflowException))
+            //*****************************************************************************************************************************************
+            //MAIN 
+            ConsoleSpiner spin = new ConsoleSpiner();
+            while (true)
             {
-                Console.WriteLine(("The file system watcher experienced an internal buffer overflow: " + e.GetException().Message));
-                Trace.WriteLine(("The file system watcher experienced an internal buffer overflow: " + e.GetException().Message));
+                System.Threading.Thread.Sleep(500);
+                try
+                {
+                    if (Buffer.Count() == 0) { Console.Write("\r System ready (buffer empty)                               "); spin.Turn(); }
+                    else
+                    {
+                        List<string> localbuffer = Buffer.getbuffer();
+                        foreach (string file in localbuffer.ToList())
+                        {
+                            try
+                            {
+                                if (Buffer.Contains(file) && IsFileReady(file)) 
+                                {
+                                    Console.WriteLine("");
+                                    HandleTsyncFileCreate(file);
+                                    Buffer.Delete(file);
+                                }
+                                else if (!File.Exists(file)) { Buffer.Delete(file); Debug.Message("FileNotExistWhileInBuffer", file.Substring(Math.Max(0, file.Length - 40))); }
+                            }
+                            catch (Exception ex) { Debug.Message("Buffersweep", file.Substring(Math.Max(0, file.Length - 40)) + " msg: " + ex.Message); }
+                        }
+                    }
+                }
+                catch (Exception ex) { Debug.Message("GeneralCatch", " msg: " + ex.Message); }
             }
-            Console.WriteLine("System will restart in 30 seconds");
-            System.Threading.Thread.Sleep(30000);
-            //restart application
-            var fileName = Assembly.GetExecutingAssembly().Location;
-            System.Diagnostics.Process.Start(fileName);
-            Environment.Exit(0);
-        }
 
- // Event handeler for robot puts file on server
-        private static void OnChanged(object source, FileSystemEventArgs e)
-        {
-            System.Threading.Thread.Sleep(1000); //anders leest hij lege file
-            string text = System.IO.File.ReadAllText(e.FullPath);
-            text = text.TrimEnd(new char[] { '\r', '\n' });
-            DateTime servertime = GetTimeFromGadata();
-            System.Console.WriteLine("Robot: " + e.Name + " Robottime= " + text + " Send: " + servertime.ToString("HH:mm:ss") + " " + servertime.ToString("d-MM-yy"));
-            System.IO.File.WriteAllText(e.FullPath, servertime.AddSeconds(16).ToString("HH:mm:ss") + Environment.NewLine + servertime.ToString("d-MM-yy"));
-            LogTsynctoGADATA(e.Name.Substring(0, 8), ConvertComauDate(text));
         }
-
+ //Event handeler for error from filewatcher
+        private static void OnError(object source, ErrorEventArgs e) { Debug.Message("FileWachter", e.GetException().Message); Debug.Restart(); }
+//Event handeler for robot creates file  (adds to buffer)
+        private static void OnChanged(object source, FileSystemEventArgs e) { Buffer.Record(e.FullPath); }
  //Event handeler for robot confirms sync
         private static void OnDeleted(object source, FileSystemEventArgs e)
         {
-            Console.WriteLine("Robot: " + e.Name + " Confirms Sync");
-            LogConfirmtoGADATA(e.Name.Substring(0,8));
+            Console.WriteLine("");
+            Debug.Message("Event", "Robot: " + e.Name + " Confirms Sync");
+            LogConfirmtoGADATA(e.Name.Substring(0, 8));
         }
-
+ // handeler for robot puts file on server
+        private static void HandleTsyncFileCreate(string fullfilepath)
+        {
+            string text = System.IO.File.ReadAllText(fullfilepath);
+            text = text.TrimEnd(new char[] { '\r', '\n' });
+            DateTime servertime = GetTimeFromGadata();
+            Debug.Message("Event", "Robot: " + Path.GetFileNameWithoutExtension(fullfilepath) + " Robottime= " + text + " Send: " + servertime.ToString("HH:mm:ss") + " " + servertime.ToString("d-MM-yy"));
+            System.IO.File.WriteAllText(fullfilepath, servertime.AddSeconds(16).ToString("HH:mm:ss") + Environment.NewLine + servertime.ToString("d-MM-yy"));
+            LogTsynctoGADATA(Path.GetFileNameWithoutExtension(fullfilepath), ConvertComauDate(text));
+        }
  //function to convert a comau date string to datetime
         static DateTime ConvertComauDate(String ad_date)
         {
@@ -97,7 +170,7 @@ namespace C3G_Timesync
             DateTime parsedDate;
             if (DateTime.TryParseExact(ad_date, pattern, new CultureInfo("en-GB"), DateTimeStyles.AllowWhiteSpaces, out parsedDate))
             {
-                Console.WriteLine("Converted '{0}' to {1}.", ad_date, parsedDate);
+               // Console.WriteLine("Converted '{0}' to {1}.", ad_date, parsedDate);
                 return parsedDate;
             }
             else
@@ -106,7 +179,7 @@ namespace C3G_Timesync
                 return parsedDate;
             }
         }
- // get time form server
+ //get time form server
         static DateTime GetTimeFromGadata()
         {
             //open connection to gadata
@@ -129,6 +202,14 @@ namespace C3G_Timesync
                         myCommand.ExecuteNonQuery();
                         Object returnValue = myCommand.ExecuteScalar();
                         DateTime GAtime = DateTime.Parse(returnValue.ToString());
+                        try
+                        {
+                            Gadataconn.Close();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.ToString());
+                        }
                         return GAtime;
                     }
                 }
@@ -136,16 +217,6 @@ namespace C3G_Timesync
                 {
                     Console.WriteLine(e.ToString());
                     return DateTime.Now;
-                }
-
- 
-                try
-                {
-                    Gadataconn.Close();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.ToString());
                 }
             }
 
@@ -231,6 +302,31 @@ namespace C3G_Timesync
             {
                 Console.WriteLine(e.ToString());
             }
+            }
+        }
+ //check if file is accesable
+        public static bool IsFileReady(String sFilename)
+        {
+            // If the file can be opened for exclusive access it means that the file
+            // is no longer locked by another process.
+            try
+            {
+                using (FileStream inputStream = File.Open(sFilename, FileMode.Open, FileAccess.Read, FileShare.None))
+                {
+                    if (inputStream.Length > 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+                return false;
             }
         }
     }
